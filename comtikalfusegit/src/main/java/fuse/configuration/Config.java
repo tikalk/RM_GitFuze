e4ef096d2e24099c44b1576f8050d0e00eb6ce0c
zeev <zeev@tikalk.com>
2017-07-10 12:48:05 +0300

    a

--- comtikalfusegit/src/main/java/fuse/configuration/Config.java
+++ comtikalfusegit/src/main/java/fuse/configuration/Config.java
@@ -1,5 +1,6 @@
 package fuse.configuration;
 
+import org.eclipse.egit.github.core.client.GitHubClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,10 +11,14 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class Config {
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+/*    @Bean
+    public GitHubClient gitHubClient() {
+        return new GitHubClient();
+    }*/
+
+
+
+
 }
 
 
--- comtikalfusegit/src/main/java/fuse/controllers/GitOperationsController.java
+++ comtikalfusegit/src/main/java/fuse/controllers/GitOperationsController.java
@@ -1,21 +1,66 @@
 package fuse.controllers;
 
+import org.eclipse.egit.github.core.PullRequest;
+import org.eclipse.egit.github.core.PullRequestMarker;
+import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.service.PullRequestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by zeev on 7/10/17.
  */
 @RestController
+@RequestMapping("fusegit")
 public class GitOperationsController {
 
     private static final Logger logger = LoggerFactory.getLogger(GitOperationsController.class);
 
-    @Autowired
-    private RestTemplate restTemplate;
+ /*   @Autowired
+    private GitHubClient gitHubClient;*/
+
+
+    @RequestMapping(value = "/pull-request", method = RequestMethod.GET)
+    @ResponseBody
+    public String gitClone() {
+        try {
+
+
+            PullRequestService service = new PullRequestService();
+            service.getClient().setCredentials("pnielab@gmail.com", "pnini007");
+            RepositoryId repo = RepositoryId.createFromUrl("https://github.com/tikalk/RM_GitFuze.git");
+            PullRequest pullRequest = new PullRequest();
+            pullRequest.setTitle("pull request");
+
+            PullRequestMarker base = new PullRequestMarker();
+            base.setLabel("master");
+            pullRequest.setBase(base);
+
+            PullRequestMarker head = new PullRequestMarker();
+            head.setLabel("dev");
+            pullRequest.setHead(head);
+
+            PullRequest request = service.createPullRequest(repo, pullRequest);
+            request = null;
+
+         /*
+
+            GitHubClient client = new GitHubClient();
+            client.setCredentials("pnielab@gmail.com", "pnini007");
+            client.
+
+            GitHub gitHub = GitHub.connect("pnielab@gmail.com","pnini07");
+            gitHub.
+            GHRepository repository = gitHub.getRepository("https://github.com/tikalk/RM_GitFuze.git");*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 
 }
